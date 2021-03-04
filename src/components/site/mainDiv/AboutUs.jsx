9fda7ff2a9466cc89a24d793f444703681186803
Emily Scudero <emily@MSI.COM>
2021-03-04 17:48:49 -0500

    last update from emily

--- src/components/site/mainDiv/AboutUs.jsx
+++ src/components/site/mainDiv/AboutUs.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link } from "react-router-dom";
+import { 
+    Button, 
+   Jumbotron
+} from 'reactstrap'; ;
 
 const AboutUs = () => {
-    return (
-        <div className="main">
-            <h1>About Us Section</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure mollitia itaque dolorum quam saepe nobis, a similique alias quidem libero veniam nesciunt odit rerum praesentium. Ratione provident excepturi ipsam ducimus.</p>
-            <p>Lorem fugiat incidunt odio ipsum dolor sit amet consectetur error fuga nihil adipisicing elit. Ipsa provident placeat repellendus reiciendis laudantium dicta ab doloribus eos quibusdam quam. Aut accusamus repellendus reiciendis  dolorum minus fugiat incidunt odio? </p>
-        </div>
-    );
-}
+  
+        return (
+            <div className="aboutus">
+            <h1>About Us</h1>
+            <img src ="https://res.cloudinary.com/dqaf1fih0/image/upload/v1614706728/photos-BBP/happypic_p5aa42.jpg"/>
+
+            <br/>
+            <br/>
+
+            <Jumbotron>
+              <h1 className="display-3">FireLogger as a Tool</h1>
+              <p className="lead">FireLogger is a useful tool for both parties in an event of a house claim.</p>
+              <hr className="my-2" />
+              <p>Keep track of your valuable belongings before life happens. See how we came about as an application.</p>
+              <p className="lead">
+              <Button color="warning">
+            <Link to="/OurStory" className="inactive">
+             Our Story
+            </Link>
+          </Button>
+              </p>
+            </Jumbotron>
+
+            <Jumbotron>
+              <h1 className="display-3">Our Founder</h1>
+              <p className="lead">In 2007, Hank Flowers founded Firelogger.</p>
+              <hr className="my-2" />
+              <p>A humble man from Illinois had a house fire while at work. Click below to read about his tramatic experience that led to a well-known application we know today.</p>
+              <p className="lead">
+              <Button color="warning">
+              <Link to="/HanksStory" className="inactive">
+             About Hank
+            </Link>
+          </Button>
+               </p>
+            </Jumbotron>
+
+
+
+
+
+
+          </div>
+        );
+      };
+      
  
 export default AboutUs;
---
+++ src/components/site/mainDiv/OurStory.js
@@ -0,0 +1,41 @@
+import React from "reactstrap";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+const OurStory = () => {
+  return (
+    <div className="ourstory">
+      <img src="https://res.cloudinary.com/dqaf1fih0/image/upload/b_rgb:f7f7f7,bo_9px_solid_rgb:f7f7f7,c_scale,co_rgb:ffffff,e_auto_contrast,r_21,w_566/v1614711313/photos-BBP/house_fire_gslytk.jpg" />
+
+      <br />
+      <br />
+      <h1>Our Story</h1>
+      <p>
+        In 2007, a man named Hank Flowers experienced one of lifes hardest
+        challenges when he got a call from the fire department that his house
+        had caught fire while he was at work. The fire had claimed about half of
+        his house and everything in it. Hank went through weeks of record
+        keeping for every valuable in his house he could remember and days of
+        research on the details to provide to his insurance company so they
+        would payout. After months of delayed contact with the claims
+        representative, Hank wondered why the information wasn't taken prior to the
+        fire and how it could have been managed better. He later came up with an
+        idea.
+      </p>
+      <p>
+        His idea sprouted the application we know today as FireLogger.
+        FireLogger was made to help not only the insurance company in resolving
+        insureds' claims like Hank's in a timely manner but also to relieve the
+        un-needed stress off the insured so they can focus on what matters most
+        to them in an unpredicatble event.
+      </p>
+
+      <Button outline color="secondary">
+        <Link to="/HanksStory" className="inactive">
+          Read about our founder
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default OurStory;
